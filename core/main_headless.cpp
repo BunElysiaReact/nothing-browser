63ef5dd6c76371af744444acadf8397cf84bd39d
Ernest12287 <peaseernest8@gmail.com>
2026-04-08 19:22:48 +0300

    fixed errors in headless

--- core/main_headless.cpp
+++ core/main_headless.cpp
@@ -1,13 +1,25 @@
-#include <QCoreApplication>
-#include "PiggyServer.h"
+#include <QApplication>
+#include <QWebEngineProfile>
+#include "engine/PiggyServer.h"
 
 int main(int argc, char *argv[]) {
-    QCoreApplication app(argc, argv);
+    // Must be set BEFORE QApplication
+    qputenv("QTWEBENGINE_CHROMIUM_FLAGS",
+        "--disable-gpu "
+        "--no-sandbox "
+        "--disable-dev-shm-usage "
+        "--disable-software-rasterizer "
+        "--headless=new "
+        "--allow-running-insecure-content"
+    );
+    qputenv("QT_QPA_PLATFORM", "offscreen");
+
+    QApplication app(argc, argv);  // NOT QCoreApplication
     app.setApplicationName("nothing-browser-headless");
 
-    PiggyServer server(nullptr); // no PiggyTab — headless
+    PiggyServer server(nullptr);
     server.start();
 
-    qDebug() << "[Piggy] Headless daemon running on socket: piggy";
+    qDebug() << "[Piggy] Headless daemon on socket: piggy";
     return app.exec();
 }
